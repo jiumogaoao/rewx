ce4e38d52c8763f95e1d0432f20823c788bf1742
jiumogaoao <394127821@qq.com>
2017-06-27 06:39:03 +0800

    change

--- modules/TalkFoot.js
+++ modules/TalkFoot.js
@@ -4,17 +4,29 @@ import {
    View,
    StyleSheet,
    TouchableHighlight,
+   ScrollView,
    TextInput
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons-iconfont/IconFont';
 
 class TalkFoot extends Component {
-   
+   renderScrollItem(list) {
+      const itemArr = [];
+      for(var i = 0; i < list.length; i++) {
+      itemArr.push(
+        <View style={{width:'25%'}}>
+          <Icon name={list[i].icon} style={{width:80,height:80,backgroundColor:'#fff',marginLeft:35,textAlign:'center',fontSize:50,lineHeight:60,marginBottom:10,borderRadius:10,borderColor:'#999',borderWidth:1}}></Icon>
+          <Text style={{textAlign:'center',marginBottom:10}}>{list[i].name}</Text>
+        </View>
+      );
+    }
+    return itemArr;
+   }
    render() {
       return (
-            <View style={{height : 500,flexDirection:'column',justifyContent : 'space-between',borderTopColor:'#000',borderTopWidth:1,backgroundColor:'#eee'}}>
-              <View style={{flex:1,flexDirection:'row',justifyContent : 'space-between'}}>
+            <View style={{height : 400,flexDirection:'column',borderTopColor:'#000',borderTopWidth:1,backgroundColor:'#eee'}}>
+              <View style={{height:100,flexDirection:'row',justifyContent : 'space-between'}}>
                 <Icon name="yuying" style={{fontSize:40,lineHeight:40,textAlign:'center',width:50,height:50,borderColor:'#000',borderWidth:1,borderRadius:50,marginLeft:10,marginTop:25}}/>
                 <TextInput
                   style={{height: 60,flex:1,backgroundColor:'#fff',marginLeft:10,marginTop:20}}
@@ -23,8 +35,17 @@ class TalkFoot extends Component {
                 <Icon name="biaoqing" style={{fontSize:40,lineHeight:40,textAlign:'center',width:50,height:50,borderColor:'#000',borderWidth:1,borderRadius:50,marginLeft:10,marginTop:25}}/>
                 <Icon name="jia" style={{fontSize:40,lineHeight:37,textAlign:'center',width:50,height:50,borderColor:'#000',borderWidth:1,borderRadius:50,marginLeft:10,marginRight:10,marginTop:25}}/>
               </View>
-              <View style={{height:400}}>
-                
+              <View style={{flex:1,flexDirection:'column'}}>
+                <ScrollView
+          //水平方向  
+       horizontal={true} pagingEnabled={true} showsHorizontalScrollIndicator={false}> 
+                  <View style={{width:600,height:300,flexDirection:'row',flexWrap:'wrap'}}>
+                    {this.renderScrollItem([{icon:'xiangche',name:'相册'},{icon:'paise',name:'拍摄'},{icon:'shiping',name:'视频聊天'},{icon:'weizhi',name:'位置'},{icon:'hongbao',name:'红包'},{icon:'zhuanzhang',name:'转账'},{icon:'wo2',name:'个人名片'},{icon:'yuying',name:'语音输入'}])}
+                  </View>
+                  <View style={{width:600,height:300,flexDirection:'row'}}>
+                    {this.renderScrollItem([{icon:'shoucang',name:'收藏'},{icon:'kaquan',name:'卡券'}])}
+                  </View>
+                </ScrollView>
               </View>
             </View>
          );
